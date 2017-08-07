632727dc6787656cef8f7c2732b25948e2ff21f1
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-08-06 22:43:09 -0500

    Displaying data the right way

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -18,7 +18,6 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         super.viewDidLoad()
         categoryTable.dataSource = self
         categoryTable.delegate = self
-        
 
         
     }
@@ -29,16 +28,28 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
-            if let category = DataService.instance.getCategories()[indexPath.row] as? Category {
+             let category = DataService.instance.getCategories()[indexPath.row]
                 cell.updateViews(category: category)
                 return cell
-            }
         } else {
             return CategoryCell()
         }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
         
-        
-        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            assert(sender as? Category != nil)
+            productsVC.initProducts(category: sender as! Category)
+        }
     }
     
 }
