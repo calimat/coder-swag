45d3eb7456c6a760443fa4ab5ee3948e60eb5da1
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-08-06 21:59:26 -0500

    Added collection view

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ricardo Herrera Petit on 8/6/17.
+//  Copyright © 2017 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
